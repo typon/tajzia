5ac510f49bb229b8f24049c98f48f46416009e1f
typon <hassan.typon@gmail.com>
2021-12-25 02:34:39 -0500

    added unit prop + more funcs

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const expect = std.testing.expect;
 const assert = std.debug.assert;
 const log = std.log;
 const print = @import("std").debug.print;
@@ -10,6 +11,7 @@ const ArrayListUnmanaged = std.ArrayListUnmanaged;
 
 const log_empty_line = utils.log_empty_line;
 const testing = std.testing;
+var rng_factory = std.rand.DefaultPrng.init(0);
 
 fn add(a: i32, b: i32) i32 {
     return a + b;
@@ -20,7 +22,7 @@ const Polarity = enum(u16) {
     negative,
 };
 
-const LiteralAssignment = enum(u8) {
+const VariableAssignment = enum(u8) {
     true_,
     false_,
     unassigned,
@@ -31,10 +33,14 @@ const Literal = packed struct {
     polarity: Polarity,
 };
 
+const VariableDecisionPolicy = enum { random };
+
 const SolverResult = enum { sat, unsat, unknown };
 
-const UnitPropagateResult = enum { found_unsat_clause, otherwise };
+const UnitPropagateResult = union(enum) { found_unsat_clause: usize, otherwise };
+
 const AnalyzeClauseResult = enum { unit, unsat, sat, non_unit };
+
 const ProblemSpec = struct {
     num_clauses: u32,
     num_variables: u32,
@@ -58,27 +64,42 @@ const ProblemSpec = struct {
     }
 };
 
+const SolverConfig = struct {
+    variable_decision_policy: VariableDecisionPolicy,
+};
+
 const CDCLSolver = struct {
+    config: SolverConfig,
     arena: std.heap.ArenaAllocator,
     allocator: Allocator,
     problem_spec: ProblemSpec,
-    literal_assignments: ArrayListUnmanaged(LiteralAssignment),
-    literal_decision_levels: ArrayListUnmanaged(i16),
+    variable_assignments: ArrayListUnmanaged(VariableAssignment),
+    variable_decision_levels: ArrayListUnmanaged(i16),
+    variable_antecedent_clause_ids: ArrayListUnmanaged(?usize),
+    num_assigned_variables: u32,
+    rng: std.rand.Random,
 
-    pub fn init(problem_spec: ProblemSpec) !CDCLSolver {
+    pub fn init(problem_spec: ProblemSpec, solver_config: SolverConfig) !CDCLSolver {
         var solver_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+
+        // Handle random number generation
+        const rng = rng_factory.random();
+
         var cdcl_solver = CDCLSolver{
+            .config = solver_config,
             .arena = solver_arena,
             .allocator = solver_arena.allocator(),
             .problem_spec = problem_spec,
-            .literal_assignments = ArrayListUnmanaged(LiteralAssignment){},
-            .literal_decision_levels = ArrayListUnmanaged(i16){},
+            .variable_assignments = ArrayListUnmanaged(VariableAssignment){},
+            .variable_decision_levels = ArrayListUnmanaged(i16){},
+            .variable_antecedent_clause_ids = ArrayListUnmanaged(?usize){},
+            .num_assigned_variables = 0,
+            .rng = rng,
         };
 
-        var i: u32 = 0;
-        while (i < problem_spec.num_variables) : (i += 1) {
-            try cdcl_solver.literal_assignments.append(cdcl_solver.allocator, LiteralAssignment.unassigned);
-        }
+        try cdcl_solver.variable_assignments.appendNTimes(cdcl_solver.allocator, VariableAssignment.unassigned, problem_spec.num_variables);
+        try cdcl_solver.variable_decision_levels.appendNTimes(cdcl_solver.allocator, -1, problem_spec.num_variables);
+        try cdcl_solver.variable_antecedent_clause_ids.appendNTimes(cdcl_solver.allocator, null, problem_spec.num_variables);
 
         return cdcl_solver;
     }
@@ -88,23 +109,36 @@ const CDCLSolver = struct {
         self.arena.deinit();
     }
 
-    pub fn solve(self: *CDCLSolver) SolverResult {
-        _ = self;
-
-        _ = self.unit_propagate(0);
-
-        return SolverResult.unknown;
+    pub fn all_variables_assigned(self: *const CDCLSolver) bool {
+        return self.num_assigned_variables == self.problem_spec.num_variables;
     }
 
-    fn analyze_clause(self: *const CDCLSolver, clause: *const ArrayListUnmanaged(Literal), unset_literal_id: *?u16) AnalyzeClauseResult {
+    fn decide_branching_variable_random_policy(self: *const CDCLSolver) u16 {
+        while (true) {
+            const variable_id = self.rng.intRangeLessThan(u16, 0, @intCast(u16, self.variable_assignments.items.len));
+            if (self.variable_assignments.items[variable_id] == VariableAssignment.unassigned) {
+                return variable_id;
+            }
+        }
+    }
+
+    fn decide_branching_variable(self: *const CDCLSolver) u16 {
+        switch (self.config.variable_decision_policy) {
+            VariableDecisionPolicy.random => {
+                return self.decide_branching_variable_random_policy();
+            },
+        }
+    }
+
+    fn analyze_clause(self: *const CDCLSolver, clause: *const ArrayListUnmanaged(Literal), unset_literal: *?Literal) AnalyzeClauseResult {
         var num_unassigned: u32 = 0;
         var num_false: u32 = 0;
 
         for (clause.items) |literal| {
-            if (self.literal_assignments.items[literal.id] == LiteralAssignment.unassigned) {
+            if (self.variable_assignments.items[literal.id] == VariableAssignment.unassigned) {
                 num_unassigned += 1;
-                unset_literal_id.* = literal.id;
-            } else if ((self.literal_assignments.items[literal.id] == LiteralAssignment.true_ and literal.polarity == Polarity.positive) or (self.literal_assignments.items[literal.id] == LiteralAssignment.false_ and literal.polarity == Polarity.negative)) {
+                unset_literal.* = literal;
+            } else if ((self.variable_assignments.items[literal.id] == VariableAssignment.true_ and literal.polarity == Polarity.positive) or (self.variable_assignments.items[literal.id] == VariableAssignment.false_ and literal.polarity == Polarity.negative)) {
                 // Return immediately if any literal in the clause is assigned true
                 return AnalyzeClauseResult.sat;
             } else {
@@ -121,20 +155,62 @@ const CDCLSolver = struct {
         }
     }
 
-    fn unit_propagate(self: *CDCLSolver, decision_level: u32) UnitPropagateResult {
-        const unit_clause_found = false;
-        _ = decision_level;
+    fn assign_value_to_literal(self: *CDCLSolver, unset_literal: Literal, decision_level: i16, clause_id: usize) void {
+        self.variable_assignments.items[unset_literal.id] = if (unset_literal.polarity == Polarity.positive)
+            VariableAssignment.true_
+        else
+            VariableAssignment.false_;
 
-        while (true) {
-            for (self.problem_spec.clauses.items) |clause| {
-                var unset_literal_id: ?u16 = null;
-                print("{} \n", .{self.analyze_clause(&clause, &unset_literal_id)});
+        self.variable_decision_levels.items[unset_literal.id] = decision_level;
+        self.variable_antecedent_clause_ids.items[unset_literal.id] = clause_id;
+
+        self.num_assigned_variables += 1;
+    }
+
+    fn unit_propagate(self: *CDCLSolver, decision_level: i16) UnitPropagateResult {
+        var unit_clause_found = false;
+
+        main_loop: while (true) {
+            // Attempt to find a unit clause, and if one is found, set it's
+            // only assigned literal to a value that causes the clause to become sat
+            for (self.problem_spec.clauses.items) |clause, clause_id| {
+                var unset_literal: ?Literal = null;
+                const clause_analysis = self.analyze_clause(&clause, &unset_literal);
+                if (clause_analysis == AnalyzeClauseResult.unit) {
+                    unit_clause_found = true;
+                    self.assign_value_to_literal(unset_literal.?, decision_level, clause_id);
+                    break :main_loop;
+                } else if (clause_analysis == AnalyzeClauseResult.unsat) {
+                    return UnitPropagateResult{ .found_unsat_clause = clause_id };
+                }
             }
             if (!unit_clause_found) {
-                return UnitPropagateResult.otherwise;
+                break :main_loop;
             }
+            // A unit clause was found, so keep going and finding more unit clauses
         }
+        return UnitPropagateResult.otherwise;
+    }
+
+    pub fn solve(self: *CDCLSolver) SolverResult {
         _ = self;
+
+        var decision_level: i16 = 0;
+
+        // Do initial unit prop to find any conflicts immediately
+        switch (self.unit_propagate(decision_level)) {
+            UnitPropagateResult.found_unsat_clause => {
+                return SolverResult.unsat;
+            },
+            UnitPropagateResult.otherwise => {},
+        }
+
+        while (!self.all_variables_assigned()) {
+            const variable_id = self.decide_branching_variable();
+            log.debug("variable: {}", .{variable_id});
+            std.time.sleep(1000_000_000);
+        }
+        return SolverResult.unknown;
     }
 };
 
@@ -216,14 +292,12 @@ test "parse dimacs cnf file" {
 
     const file_path = "test.cnf";
     const problem_spec: ProblemSpec = try parse_dimacs_cnf_file(allocator, file_path);
-
     problem_spec.dump();
 
-    // var cdcl_solver: CDCLSolver = .{ .arena = solver_arena, .allocator = solver_arena.allocator(), .problem_spec = problem_spec, .decision_level = 0 };
-    // defer _ = cdcl_solver.problem_spec.arena.deinit();
-    // defer _ = cdcl_solver.arena.deinit();
-    var cdcl_solver = try CDCLSolver.init(problem_spec);
+    const solver_config = SolverConfig{ .variable_decision_policy = VariableDecisionPolicy.random };
+
+    var cdcl_solver = try CDCLSolver.init(problem_spec, solver_config);
     defer cdcl_solver.deinit();
 
-    _ = cdcl_solver.solve();
+    try expect(cdcl_solver.solve() == SolverResult.unknown);
 }
